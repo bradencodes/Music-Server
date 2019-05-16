59aff9060e82efbf84a8bfebd0ba02da7f8069a3
Braden Walker <bradencodes@gmail.com>
2019-05-16 12:12:25 -0700

    check that username is between 5 and 25 chars

--- register.php
+++ register.php
@@ -4,7 +4,6 @@
     include("includes/classes/Account.php");
 
     $account = new Account();
-    $account->register();
 
     include("includes/handlers/register-handler.php");
     include("includes/handlers/login-handler.php");
