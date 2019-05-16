6cff697b53a0567b8d0f49dce94b5468322374d1
Braden Walker <bradencodes@gmail.com>
2019-05-16 10:27:04 -0700

    add register page

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Slotify!</title>
+</head>
+<body>
+    <div id="inputContainer">
+        <form id="loginForm" action="register.php" method="POST">
+            <h2>Login to your account</h2>
+            <p>
+                <label for="loginUsername">Username</label>
+                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+            </p>
+            <p>
+                <label for="loginPassword">Password</label>
+                <input id="loginPassword" name="loginPassword" type="password" required>
+            </p>
+
+            <button type="submit" name="loginButton">Log In</button>
+        </form>
+    </div>
+</body>
+</html>
