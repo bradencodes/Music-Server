5ebad340406138f555caaee126fde6342572e80a
Braden Walker <bradencodes@gmail.com>
2019-05-16 16:44:30 -0700

    login error handling

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -11,8 +11,11 @@
         public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
 
         public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
-        
+
         public static $usernameCharacters = "Your username must be between 5 and 25 characters";
         public static $usernameTaken = "This username is already in use";
+
+
+        public static $loginFailed = "Invalid Credentials";
     }
 ?>
