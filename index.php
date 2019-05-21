bec979a63680876b62c153265e24b65e0fa125ca
Braden Walker <bradencodes@gmail.com>
2019-05-21 12:43:03 -0700

    style nav bar

--- index.php
+++ index.php
@@ -35,6 +35,28 @@ else {
                         <img src="assets/images/icons/logo.png" alt="logo">
                     </a>
 
+                    <div class="group">
+
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search
+                                <img src="assets/images/icons/search.png" class="icon" alt="search">
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Braden Walker</a>
+                        </div>
+                    </div>
+
                 </nav>
             </div>
 
