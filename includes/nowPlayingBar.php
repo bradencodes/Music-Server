64c1e1f3e46622716d7e97e1511d0665eb83d0d3
Braden Walker <bradencodes@gmail.com>
2019-05-22 10:44:44 -0700

    prevent highlighting player bar

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -20,6 +20,10 @@
         setTrack(currentPlaylist[0], currentPlaylist, false);
         updateVolumeProgressBar(audioElement.audio);
 
+        $("#nowPlayingBarContainer").on("mousedown touchstart mousemove touchmove", function(e) {
+            e.preventDefault();
+        });
+
         //Progress Bar
         $(".playbackBar .progressBar").mousedown(function() {
             mouseDown = true;
