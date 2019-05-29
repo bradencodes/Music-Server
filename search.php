8ad43b71a95f02e855d4ccbcfb5b6f743db49283
Braden Walker <bradencodes@gmail.com>
2019-05-29 11:40:36 -0700

    search updates query

--- search.php
+++ search.php
@@ -1,16 +1,37 @@
 <?php
-include("includes/includedFiles.php");
-if(isset($_GET['term'])) {
-    $term = urldecode( $_GET['term'] );
-}
-else {
-    $term = "";
-}
+
+    include("includes/includedFiles.php");
+    if(isset($_GET['term'])) {
+        $term = urldecode( $_GET['term'] );
+    }
+    else {
+        $term = "";
+    }
+
 ?>
 
 <div class="searchContainer">
 
     <h4>Search for an artist, album, or song</h4>
-    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
+    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="this.setSelectionRange(1000,1001);">
 
 </div>
+
+<script>
+
+    $(".searchInput").focus();
+
+    $(function() {
+        var timer;
+
+        $(".searchInput").keyup(function() {
+            clearTimeout(timer);
+
+            timer = setTimeout(() => {
+                var val = $(".searchInput").val();
+                openPage("search.php?term=" + val);
+            }, 500);
+        })
+    })
+
+</script>
